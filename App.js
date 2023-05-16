466e1c51d812bd66d56401090615daf81d59c832
SAYYED MOHD ARIF <122776196+mrmohdarif@users.noreply.github.com>
2023-05-16 17:41:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Feedback from './Feedbackorm/Feedback';
+import './Style.css'
+
+
+function App() {
+ 
+  
+  return (
+    <div className="App">
+     <Feedback />  
+    </div>
+  );
+}
+
+export default App;
